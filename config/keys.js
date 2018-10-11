b0def3f468ed8c3afb2c5d89386ccb061d9bf7b5
Luke Dalton <luke.dalton17@gmail.com>
2018-10-10 23:10:26 -0500

    Fixing possible sytax error in keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // keys.js - figure out what set of creds to return
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   //we are in prod - return prod set of keys
-  module.exports = require('./prod');
+  module.exports = require("./prod");
 } else {
   //we are in dev - return dev set of keys
-  module.exports = require('./dev');
+  module.exports = require("./dev");
 }
